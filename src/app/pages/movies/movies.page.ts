211d9ee4ada7efd1cb971e5eef10ac3ce7bb9ded
Luis José Sánchez <luisjoseprofe@gmail.com>
2019-10-21 23:46:37 +0200

    Updated app

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService, SearchType } from 'src/app/services/movie.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-movies',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesPage implements OnInit {
 
-  constructor() { }
+  results: Observable<any>;
+  searchTerm: string = '';
+  type: SearchType = SearchType.all;
+
+  constructor(private movieService: MovieService) { }
 
   ngOnInit() {
+  }
+
+  searchChanged() {
+    this.results = this.movieService.searchData(this.searchTerm, this.type);
   }
 
 }
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export enum SearchType {
+  all = '',
+  movie = 'movie',
+  series = 'series',
+  episode = 'episode'
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  constructor() { }
+  url = 'http://www.omdbapi.com/';
+  apiKey = 'ef421f54';
+
+  constructor(private http: HttpClient) { }
+
+  searchData(title: string, type: SearchType): Observable<any> {
+    return this.http.get(
+      `${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`
+    );
+  }
+
+  getDetails(id: number) {
+    return this.http.get(`${this.url}?i=${id}&plot=full&apikey=${this.apiKey}`);
+  }
 }
